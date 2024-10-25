33d67457e2f582f88f8a05e563f2adf44f30e256
ghaithkhl27 <ghaithkhalfallah27@hotmail.com>
2024-10-25 14:43:43 +0100

    I made a schema.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,18 @@
-const express=require('express')
-const app =express()
-app.use(express.json())
-app.listen(4000,()=>console.log('server runing on port 4000'));
+const express = require('express');
+const bodyParser = require('body-parser');
+const dishRoutes = require('./Routes/dishRoutes.js');
+
+const app = express();
+app.use(bodyParser.json());
+
+
+app.use(dishRoutes);
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
