3c9ef91c3465021a61a5e53d2fac7e462db72d5b
Shayma1334 <habibishayma21@gmail.com>
2024-10-28 18:22:00 +0100

    Delete backend directory

--- backend/Routes/dishRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const dishController = require('../Controllers/dishController');
-
-const router = express.Router();
-
-// Route to get all dishes
-router.get('/dishes', dishController.getAllDishes);
-
-module.exports = router;
