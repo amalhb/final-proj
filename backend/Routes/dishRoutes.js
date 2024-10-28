08b720ba611391d27aa878c69840ea4a41382357
ghaithkhl27 <ghaithkhalfallah27@hotmail.com>
2024-10-28 11:31:47 +0100

    test

--- backend/Routes/dishRoutes.js
+++ backend/Routes/dishRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const dishController = require('../Controller/dishController.js');
+const dishController = require('../Controllers/dishController');
 
 const router = express.Router();
 
-// POST route to create a dish
-router.post('/dishes', dishController.createDish);
+// Route to get all dishes
+router.get('/dishes', dishController.getAllDishes);
 
 module.exports = router;
