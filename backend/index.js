4e30740e4a6abd29d72d27a911d6757167cc08bd
amalhb <hbamal251@gmail.com>
2024-10-28 11:23:15 -0700

    FIX

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dishRoutes = require('./Routes/dishRoutes.js');
+const orderRoutes =require('./Routes/orderRoute.js')
 
 const app = express();
 app.use(bodyParser.json());
-
-
 app.use(dishRoutes);
+
+
+
+
+app.use('/api/orders', orderRoutes);
 
 
 app.get('/', (req, res) => {
